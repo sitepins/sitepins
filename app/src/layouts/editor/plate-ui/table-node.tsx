e7121160dc5d80939b6dbf5a2eea77691b6cc54a
tfsomrat <themefisher.dev@gmail.com>
2026-09-14 13:57:42 +0600

    chore: update dependencies and add null safety checks to table node drop handler

--- app/src/layouts/editor/plate-ui/table-node.tsx
+++ app/src/layouts/editor/plate-ui/table-node.tsx
@@ -996,6 +996,8 @@ export function TableRowElement({
     element,
     type: element.type,
     canDropNode: ({ dragEntry, dropEntry }) =>
+      !!dragEntry &&
+      !!dropEntry &&
       PathApi.equals(
         PathApi.parent(dragEntry[1]),
         PathApi.parent(dropEntry[1]),
